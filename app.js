d0981aa80d8b4a1b6c1256d2209d93752bf0a680
Łukasz <nipseyyo@gmail.com>
2022-12-08 23:46:24 +0100

    Add operate function

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ function multiply(a,b){
 function divide(a,b){
     return a / b;
 }
+
+
+function operate(a,b,operator){
+    switch (operator) {
+        case "+":
+            add(a,b);
+            break;
+        case "-":
+            subtract(a,b);
+            break;
+        case "*":
+            multiply(a,b);
+            break;
+        case "/":
+            divide(a,b);
+            break;
+    }
+}
+
+operate(2,2,"+");
