789fcf89bca2580745dffba5669af5b9b8d5de1b
Łukasz <nipseyyo@gmail.com>
2022-12-25 17:31:29 +0100

    unnecessary const removed

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const displaySum = document.getElementById('displaySum');
 const allClear = document.getElementById('allClear');
 const clear = document.getElementById('clear');
 const modulo = document.getElementById('modulo');
-const divideBtn = document.getElementById('divideBtn');
-const addBtn = document.getElementById('addBtn');
-const subtractBtn = document.getElementById('subtractBtn');
-const multiplyBtn = document.getElementById('multiplyBtn');
 const equalBtn = document.getElementById('equalBtn');
 const buttons = document.querySelectorAll('.btn');
 const operators = document.querySelectorAll('[data-operator]');
@@ -24,10 +20,6 @@ operators.forEach((button) => {
     button.addEventListener('click', evaluate)
 })
 
-// divideBtn.onclick = (e) => evaluate(e);
-// addBtn.onclick = (e) => evaluate(e);
-// subtractBtn.onclick = (e) => evaluate(e);
-// multiplyBtn.onclick = (e) => evaluate(e);
 equalBtn.onclick = () => sum();
 allClear.onclick = () => {
     firstNum = '';
@@ -37,6 +29,35 @@ allClear.onclick = () => {
     displayNum.textContent = '';
     displaySum.textContent = '0';
 }
+
+
+function displayNumbers(e){
+    displayValue += e.target.dataset.value;
+    displaySum.textContent = displayValue;
+}
+
+function evaluate(e){
+    // firstNum = displaySum.textContent;
+    firstNum = displayValue;
+    console.log(e)
+    operatorNum = e.target.innerText;
+    displayNum.textContent = `${firstNum} ${operatorNum}`;
+    displayValue = '';
+    console.log(secondNum)
+    displaySum.textContent = secondNum;
+    
+}
+
+
+function sum(){
+    secondNum = displaySum.textContent;
+    displayNum.textContent = `${firstNum} ${operatorNum} ${secondNum}`;
+    console.log(secondNum, firstNum, operatorNum)
+    // displaySum.textContent = operate(firstNum, secondNum, operatorNum)
+    let sumDigits = operate(operatorNum, firstNum, secondNum)
+    displaySum.textContent = sumDigits;
+}
+
 
 function add(a,b){
     return a + b;
@@ -57,10 +78,8 @@ function divide(a,b){
 function operate(operator, a, b){
     a = Number(a);
 	b = Number(b);
-    console.log(operator)
     switch (operator) {
         case "+":
-            console.log(operator,a,b)
             return add(a,b);
         case "-":
             return subtract(a,b);
@@ -69,32 +88,4 @@ function operate(operator, a, b){
         case "/":
             return divide(a,b);
     }
-}
-
-function displayNumbers(e){
-    displayValue += e.target.dataset.value;
-    displaySum.textContent = displayValue;
-}
-
-function evaluate(e){
-    console.log(e)
-    firstNum = displaySum.textContent;
-    console.log("first num", firstNum)
-    operatorNum = e.target.innerText;
-    displayNum.textContent = `${firstNum} ${operatorNum}`;
-    
-    displayValue = '';
-    console.log(secondNum)
-    displaySum.textContent = secondNum;
-    
-}
-
-
-function sum(){
-    secondNum = displaySum.textContent;
-    displayNum.textContent = `${firstNum} ${operatorNum} ${secondNum}`;
-    console.log(secondNum, firstNum, operatorNum)
-    // displaySum.textContent = operate(firstNum, secondNum, operatorNum)
-    let sumDigits = operate(operatorNum, firstNum, secondNum)
-    displaySum.textContent = sumDigits;
 }
